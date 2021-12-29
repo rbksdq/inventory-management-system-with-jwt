7ca7b0974276a8ee9c833907572ddee252aaeb34
Rabeeka Sadiq <rabeeka.sadiq@10pearls.com>
2021-12-29 10:16:46 +0500

    just pushing project

---
+++ src/main/java/com/inventorywithjwt/inventorymanagementwithjwt/InventoryManagementWithJwtApplication.java
@@ -0,0 +1,13 @@
+package com.inventorywithjwt.inventorymanagementwithjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementWithJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagementWithJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/inventorywithjwt/inventorymanagementwithjwt/InventoryManagementWithJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventorywithjwt.inventorymanagementwithjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementWithJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
